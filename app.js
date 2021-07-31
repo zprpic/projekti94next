a1d3f18d880473277ef1033f06760c32be1968aa
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-07-31 13:02:32 +0200

    events and streams...

--- app.js
+++ app.js
@@ -1,43 +1,16 @@
-const { readFile, writeFile } = require("fs").promises;
-/* const util = require("util");
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile); */
+const http = require("http");
+const fs = require("fs");
 
-/*  */
-
-const start = async () => {
-  try {
-    const first = await readFile("./content/first.txt", "utf8");
-    const second = await readFile("./content/second.txt", "utf8");
-    await writeFile(
-      "./content/result-mind-grenade.txt",
-      `THIS IS AWESOME: ${(first, second)}`,
-      { flag: "a" }
-    );
-    console.log(first, second);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
-
-/* const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
+http
+  .createServer(function (req, res) {
+    /*     const text = fs.readFileSync("./content/big.txt", "utf8");
+    res.end(text); */
+    const fileStream = fs.createReadStream("./content/big.txt", "utf8");
+    fileStream.on("open", () => {
+      fileStream.pipe(res);
     });
-  });
-}; */
-
-/* getText("./content/first.txt")
-  .then((result) => {
-    console.log(result);
+    fileStream.on("error", (err) => {
+      res.end(err);
+    });
   })
-  .catch((err) => {
-    console.log(err);
-  }); */
+  .listen(5000);
